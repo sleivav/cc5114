dcd548b7e240a923e391efe173bca1d325f279e6
sleivav <sergio.leiva@ing.uchile.cl>
2019-08-01 17:44:42 -0400

    Working NAND and OR perceptrons

--- src/perceptron/AndPerceptron.kt
+++ src/perceptron/AndPerceptron.kt
@@ -1,7 +1,7 @@
 package perceptron
 
 class AndPerceptron: Perceptron {
-    fun calculateOutput(inputs: List<Int>): Int {
-        return super.calculateOutput(inputs, listOf(1.0, 1.0),-1.5)
+    fun calculateOutput(input1: Int, input2: Int): Int {
+        return super.calculateOutput(listOf(input1, input2), listOf(1.0, 1.0),-1.5)
     }
 }
---
+++ src/perceptron/NandPerceptron.kt
@@ -0,0 +1,7 @@
+package perceptron
+
+class NandPerceptron: Perceptron {
+    fun calculateOutput(input1: Int, input2: Int): Int {
+        return super.calculateOutput(listOf(input1, input2), listOf(-1.0, -1.0),1.5)
+    }
+}
