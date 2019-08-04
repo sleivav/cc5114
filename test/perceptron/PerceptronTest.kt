e79ea919e13c6f65935fb6c157f837eeb981ffbc
sleivav <sergio.leiva@ing.uchile.cl>
2019-08-04 18:07:01 -0400

    Reorder assert arguments

--- test/perceptron/PerceptronTest.kt
+++ test/perceptron/PerceptronTest.kt
@@ -7,25 +7,25 @@ internal class PerceptronTest {
     @Test
     fun andPerceptronTest() {
         val andPerceptron = AndPerceptron()
-        assertEquals(andPerceptron.calculateOutput(0,0), 0, "AND between two 0s should be 0")
-        assertEquals(andPerceptron.calculateOutput(1,1), 1, "AND between two 1s should be 1")
-        assertEquals(andPerceptron.calculateOutput(0,1), 0, "AND between 0 and 1 should be 0")
-        assertEquals(andPerceptron.calculateOutput(1,0), 0, "AND between 1 and 0 should be 0 (commutation)")
+        assertEquals(0, andPerceptron.calculateOutput(0,0), "AND between two 0s should be 0")
+        assertEquals(1, andPerceptron.calculateOutput(1,1), "AND between two 1s should be 1")
+        assertEquals(0, andPerceptron.calculateOutput(0,1), "AND between 0 and 1 should be 0")
+        assertEquals(0, andPerceptron.calculateOutput(1,0), "AND between 1 and 0 should be 0 (commutation)")
     }
     @Test
     fun orPerceptronTest() {
         val orPerceptron = OrPerceptron()
-        assertEquals(orPerceptron.calculateOutput(0,0), 0, "OR between two 0s should be 0")
-        assertEquals(orPerceptron.calculateOutput(1,1), 1, "OR between two 1s should be 1")
-        assertEquals(orPerceptron.calculateOutput(0,1), 1, "OR between 0 and 1 should be 1")
-        assertEquals(orPerceptron.calculateOutput(1,0), 1, "OR between 1 and 0 should be 1 (commutation)")
+        assertEquals(0, orPerceptron.calculateOutput(0,0), "OR between two 0s should be 0")
+        assertEquals(1, orPerceptron.calculateOutput(1,1), "OR between two 1s should be 1")
+        assertEquals(1, orPerceptron.calculateOutput(0,1), "OR between 0 and 1 should be 1")
+        assertEquals(1, orPerceptron.calculateOutput(1,0), "OR between 1 and 0 should be 1 (commutation)")
     }
     @Test
     fun nandPerceptronTest() {
         val nandPerceptron = NandPerceptron()
-        assertEquals(nandPerceptron.calculateOutput(0,0), 1, "NAND between two 0s should be 1")
-        assertEquals(nandPerceptron.calculateOutput(1,1), 0, "NAND between two 1s should be 0")
-        assertEquals(nandPerceptron.calculateOutput(0,1), 1, "NAND between 0 and 1 should be 1")
-        assertEquals(nandPerceptron.calculateOutput(1,0), 1, "NAND between 1 and 0 should be 1 (commutation)")
+        assertEquals(1, nandPerceptron.calculateOutput(0,0), "NAND between two 0s should be 1")
+        assertEquals(0, nandPerceptron.calculateOutput(1,1), "NAND between two 1s should be 0")
+        assertEquals(1, nandPerceptron.calculateOutput(0,1), "NAND between 0 and 1 should be 1")
+        assertEquals(1, nandPerceptron.calculateOutput(1,0), "NAND between 1 and 0 should be 1 (commutation)")
     }
 }
--- test/sumCircuit/SumCircuitTest.kt
+++ test/sumCircuit/SumCircuitTest.kt
@@ -11,16 +11,16 @@ internal class SumCircuitTest {
         val sumCircuit3 = SumCircuit(1 ,0)
         val sumCircuit4 = SumCircuit(1 ,1)
 
-        assertEquals(sumCircuit1.getSumResult(), 0, "Adding two 0s should return 0")
-        assertEquals(sumCircuit1.getCarryBit(), 0, "Adding two 0s shouldn't return a carry bit")
+        assertEquals(0, sumCircuit1.getSumResult(), "Adding two 0s should return 0")
+        assertEquals(0, sumCircuit1.getCarryBit(), "Adding two 0s shouldn't return a carry bit")
 
-        assertEquals(sumCircuit2.getSumResult(), 1, "Adding 1 and 0 should return 1")
-        assertEquals(sumCircuit2.getCarryBit(), 0, "Adding 1 and 0 shouldn't set the carry bit")
+        assertEquals(1, sumCircuit2.getSumResult(), "Adding 1 and 0 should return 1")
+        assertEquals(0, sumCircuit2.getCarryBit(), "Adding 1 and 0 shouldn't set the carry bit")
 
-        assertEquals(sumCircuit3.getSumResult(), 1, "Adding 1 and 0 should return 1")
-        assertEquals(sumCircuit3.getCarryBit(), 0, "Adding 1 and 0 shouldn't set the carry bit")
+        assertEquals(1, sumCircuit3.getSumResult(), "Adding 1 and 0 should return 1")
+        assertEquals(0, sumCircuit3.getCarryBit(), "Adding 1 and 0 shouldn't set the carry bit")
 
-        assertEquals(sumCircuit4.getSumResult(), 0, "Adding two 1s should return 0 (result is 10)")
-        assertEquals(sumCircuit4.getCarryBit(), 1, "Adding two 1s should set the carry bit")
+        assertEquals(0, sumCircuit4.getSumResult(), "Adding two 1s should return 0 (result is 10)")
+        assertEquals(1, sumCircuit4.getCarryBit(), "Adding two 1s should set the carry bit")
     }
 }
