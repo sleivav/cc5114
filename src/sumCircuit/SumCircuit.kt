3f29bf19674cd55972694eb2fe2c7a8778373e48
sleivav <sergio.leiva@ing.uchile.cl>
2019-08-04 18:03:35 -0400

    Add sum circuit class methods

--- src/sumCircuit/SumCircuit.kt
+++ src/sumCircuit/SumCircuit.kt
@@ -1,4 +1,23 @@
 package sumCircuit
 
-class SumCircuit(val x: Int, val y: Int) {
+import perceptron.NandPerceptron
+
+class SumCircuit(private val x: Int, private val y: Int) {
+    private val calculatedSum: Int by lazy {
+        val nandPerceptron = NandPerceptron()
+        nandPerceptron.calculateOutput(nandPerceptron.calculateOutput(x, nandPerceptron.calculateOutput(x, y)),
+                                       nandPerceptron.calculateOutput(nandPerceptron.calculateOutput(x, y), y))
+    }
+    private val calculatedCarryBit: Int by lazy {
+        val nandPerceptron = NandPerceptron()
+        nandPerceptron.calculateOutput(nandPerceptron.calculateOutput(x, y), nandPerceptron.calculateOutput(x, y))
+    }
+
+    fun getSumResult(): Int {
+        return this.calculatedSum
+    }
+
+    fun getCarryBit(): Int {
+        return this.calculatedCarryBit
+    }
 }
