68cc450b43851ae5d2dcd33d1f4def99c91aa84c
sleivav <sergio.leiva@ing.uchile.cl>
2019-08-26 22:41:41 -0400

    fix error on bias calc and return output

--- src/neuron/LearningNeuron.kt
+++ src/neuron/LearningNeuron.kt
@@ -4,7 +4,7 @@ import kotlin.properties.Delegates
 import kotlin.random.Random
 
 abstract class LearningNeuron(weightsNumber: Int, learningRate: Double) {
-    private var weights: MutableList<Double> by Delegates.notNull()
+    private var weights: MutableList<Double> = mutableListOf()
     private var learningRate: Double by Delegates.notNull()
     private val rng: Random = Random(5114)
     private var bias: Double = rng.nextDouble(-2.0, 2.0)
@@ -23,12 +23,13 @@ abstract class LearningNeuron(weightsNumber: Int, learningRate: Double) {
         for ((index, weight) in this.weights.withIndex()) {
             this.weights[index] = weight + this.learningRate * inputList[index] * diff
         }
-        this.bias += this.bias + this.learningRate * diff
+        this.bias += this.learningRate * diff
     }
 
-    fun calculateOutputAndLearn(inputList: List<Double>, desiredOutput: Double) {
+    fun calculateOutputAndLearn(inputList: List<Double>, desiredOutput: Double): Double {
         require(inputList.size==this.weights.size) {"El número de inputs pasados no es correcto para esta neurona"}
         val output = this.calculateOutput(inputList, this.weights, this.bias)
         this.learnFromOutput(inputList, output, desiredOutput)
+        return output
     }
 }
