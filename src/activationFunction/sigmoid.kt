0f4b053ef522e9b7a8122c593f787c7ca4ecbfd9
sleivav <sergio.leiva@ing.uchile.cl>
2019-08-29 15:08:39 -0400

    add more activation functions

---
+++ src/activationFunction/sigmoid.kt
@@ -0,0 +1,13 @@
+package activationFunction
+
+import kotlin.math.exp
+
+class sigmoid : ActivationFunction {
+    override fun apply(x: Double): Double {
+        return 1.0 / (1.0 + exp(x))
+    }
+
+    override fun derivative(x: Double): Double {
+        return this.apply(x) * (1 * this.apply(x))
+    }
+}
---
+++ src/activationFunction/tanh.kt
@@ -0,0 +1,14 @@
+package activationFunction
+
+import kotlin.math.exp
+import kotlin.math.pow
+
+class tanh : ActivationFunction {
+    override fun apply(x: Double): Double {
+        return (exp(x) - exp(-x)/(exp(x) + exp(-x)))
+    }
+
+    override fun derivative(x: Double): Double {
+        return 1 - apply(x).pow(2)
+    }
+}
