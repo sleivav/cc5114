4db6d89d047e68a34dcf718a5d3661cc48095b09
sleivav <sergio.leiva@ing.uchile.cl>
2019-08-29 12:50:04 -0400

    refactor activation functions to  classes

--- test/perceptron/PerceptronTest.kt
+++ test/perceptron/PerceptronTest.kt
@@ -1,5 +1,6 @@
 package perceptron
 
+import activationFunction.step
 import org.junit.Test
 import kotlin.test.assertEquals
 
@@ -7,7 +8,7 @@ import kotlin.test.assertEquals
 internal class PerceptronTest {
     @Test
     fun andPerceptronTest() {
-        val andPerceptron = AndPerceptron()
+        val andPerceptron = AndPerceptron(step())
         assertEquals(0.0, andPerceptron.calculateOutput(0.0,0.0), "AND between two 0s should be 0")
         assertEquals(1.0, andPerceptron.calculateOutput(1.0,1.0), "AND between two 1s should be 1")
         assertEquals(0.0, andPerceptron.calculateOutput(0.0,1.0), "AND between 0 and 1 should be 0")
@@ -15,7 +16,7 @@ internal class PerceptronTest {
     }
     @Test
     fun orPerceptronTest() {
-        val orPerceptron = OrPerceptron()
+        val orPerceptron = OrPerceptron(step())
         assertEquals(0.0, orPerceptron.calculateOutput(0.0,0.0), "OR between two 0s should be 0")
         assertEquals(1.0, orPerceptron.calculateOutput(1.0,1.0), "OR between two 1s should be 1")
         assertEquals(1.0, orPerceptron.calculateOutput(0.0,1.0), "OR between 0 and 1 should be 1")
@@ -23,7 +24,7 @@ internal class PerceptronTest {
     }
     @Test
     fun nandPerceptronTest() {
-        val nandPerceptron = NandPerceptron()
+        val nandPerceptron = NandPerceptron(step())
         assertEquals(1.0, nandPerceptron.calculateOutput(0.0,0.0), "NAND between two 0s should be 1")
         assertEquals(0.0, nandPerceptron.calculateOutput(1.0,1.0), "NAND between two 1s should be 0")
         assertEquals(1.0, nandPerceptron.calculateOutput(0.0,1.0), "NAND between 0 and 1 should be 1")
