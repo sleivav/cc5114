ecbcaeb61d69c8e77db928d6497037a926b8baf8
sleivav <sergio.leiva@ing.uchile.cl>
2019-08-31 18:11:42 -0400

    fix sigmoid function

--- src/activationFunction/sigmoid.kt
+++ src/activationFunction/sigmoid.kt
@@ -4,7 +4,7 @@ import kotlin.math.exp
 
 class sigmoid : ActivationFunction {
     override fun apply(x: Double): Double {
-        return 1.0 / (1.0 + exp(x))
+        return 1.0 / (1.0 + exp(-x))
     }
 
     override fun derivative(x: Double): Double {
