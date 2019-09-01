a5e38b06155fa800e6c3023e62da67b7d6b4cb0a
sleivav <sergio.leiva@ing.uchile.cl>
2019-08-31 22:35:28 -0400

    Add KDoc to ClassificationLine

--- src/classificationLine/ClassificationLine.kt
+++ src/classificationLine/ClassificationLine.kt
@@ -1,6 +1,18 @@
 package classificationLine
 
+/**
+ * Clase que representa una ecuación de la recta, para probar y entrenar un perceptrón
+ *
+ * @property m La pendiente de la recta
+ * @property n Intersección con el eje Y
+ */
 class ClassificationLine(private val m: Double, private val n: Double) {
+    /**
+     * Función para saber si un punto está sobre o bajo la recta
+     *
+     * @param x Coordenada x del punto
+     * @param y Coordenada y del punto
+     */
     fun pointPosition(x: Double, y: Double): Double {
         return when (y > this.m *x + this.n) {
             true -> 1.0
