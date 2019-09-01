aad68cb87fe993a22fd353fad8e7110e7fd9e087
sleivav <sergio.leiva@ing.uchile.cl>
2019-09-01 16:27:36 -0400

    Add KDoc to NeuralNetwork class

--- src/neuralNetwork/NeuralNetwork.kt
+++ src/neuralNetwork/NeuralNetwork.kt
@@ -5,6 +5,13 @@ import networkExperiment.TrainingSet
 import kotlin.math.pow
 import kotlin.random.Random
 
+/**
+ * Representación de una red neuronal
+ *
+ * @param layers número de capas de la red
+ * @param neuronsByLayer número de neuranas por cada capa de la red
+ * @property numberOfInputs número de entradas de la red
+ */
 class NeuralNetwork(layers: Int, neuronsByLayer: List<Int>, val numberOfInputs: Int) {
     val layersList: MutableList<NeuralLayer> = mutableListOf()
     var output: Int = 0
@@ -13,6 +20,7 @@ class NeuralNetwork(layers: Int, neuronsByLayer: List<Int>, val numberOfInputs: 
     val rng: Random = Random(1234)
 
     init {
+        require(layers == neuronsByLayer.size)
         layersList.add(NeuralLayer(neuronsByLayer[0], null))
         for(i in 1 until layers) {
             layersList.add(NeuralLayer(neuronsByLayer[i], layersList[i-1]))
@@ -20,33 +28,60 @@ class NeuralNetwork(layers: Int, neuronsByLayer: List<Int>, val numberOfInputs: 
         }
     }
 
+    /**
+     * Configura la función de activación de una capa de la red
+     *
+     * @param function función de activación
+     * @param layer capa a configurar
+     * @param layerWeights cantidad de pesos a configurar en la capa
+     * @param learningRate tasa de aprendizaje para la capa
+     */
     fun configureLayerFunction(function: ActivationFunction, layer: Int, layerWeights: Int, learningRate: Double) {
         layersList[layer].configureLayerFunction(function, layerWeights, learningRate)
     }
 
+    /**
+     * Retorna el resultado de la red al darle [input] como entrada
+     */
     fun feed(input: List<Double>): List<Double> {
         return layersList[0].feed(input)
     }
 
+    /**
+     * Entrena la red por un epoch completo, dada una entrada como [input] y un resultado esperado en [expectedOutput]
+     */
     fun train(input: List<Double>, expectedOutput: List<Double>) {
         output = predict(input)
         backPropagate(expectedOutput)
         updateWeight(input)
     }
 
+    /**
+     * Comienza la propagación de errores desde la última capa de la red
+     */
     fun backPropagate(expectedOutput: List<Double>) {
         layersList.last().backPropagate(expectedOutput)
     }
 
+    /**
+     * Entrega el resultado al darle una entrada [input] a la red
+     */
     fun predict(inputs: List<Double>): Int {
         val outputs = feed(inputs)
         return outputs.indexOf(outputs.max())
     }
 
+    /**
+     * Actualiza los pesos de las capas, dado [input]
+     */
     fun updateWeight(input: List<Double>) {
         layersList[0].updateWeights(input)
     }
 
+    /**
+     * Entrena la red una cantidad [epochs] de veces, utilizando como entrada los [trainingSets]. Actualiza la precisión
+     * y error de la red, para poder graficarlos más tarde
+     */
     fun trainEpochs(epochs: Int, trainingSets: List<TrainingSet>) {
         for (i in 0 until epochs) {
             var correctPredictions = 0
