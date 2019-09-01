a02803d6d78c7433a0f2b5b2c2bf2fcc7ef488d2
sleivav <sergio.leiva@ing.uchile.cl>
2019-09-01 16:39:44 -0400

    Add KDoc to perceptrons

--- src/perceptron/OrPerceptron.kt
+++ src/perceptron/OrPerceptron.kt
@@ -2,6 +2,9 @@ package perceptron
 
 import activationFunction.ActivationFunction
 
+/**
+ * Perceptron que calcula un OR lógico entre dos entradas
+ */
 class OrPerceptron(override val function: ActivationFunction) : Perceptron {
     fun calculateOutput(input1: Double, input2: Double): Double {
         return super.calculateOutput(listOf(input1, input2), listOf(1.0, 1.0),0.0)
--- src/perceptron/Perceptron.kt
+++ src/perceptron/Perceptron.kt
@@ -5,6 +5,9 @@ import activationFunction.ActivationFunction
 interface Perceptron {
     val function: ActivationFunction
 
+    /**
+     * Aplica la función de activación del perceptron, y devuelve el resultado
+     */
     fun calculateOutput(inputList: List<Double>, weightList: List<Double>, bias: Double): Double {
         return this.function.apply(inputList.zip(weightList) { x, w -> x*w }.sum() + bias)
     }
